55ae51bf8f327ca5ea1755ed1d2ad8399811d4e8
InnsmouthFisherman <clownkrusty124845@gmail.com>
2023-05-23 15:47:09 +0500

    initial commit

--- app/src/main/java/com/example/tickets/Ticket.java
+++ app/src/main/java/com/example/tickets/Ticket.java
@@ -1,48 +1,36 @@
 package com.example.tickets;
 
 public class Ticket {
+    int tickets=1;
+    int child_tickets=0;
+    double id;
     int price;
     String date;
     String time;
-    public static class ATicket extends Ticket{
+    public static class ATicket extends Ticket {
 
-        public ATicket(char type, double id){
+        public ATicket(int child_tickets, int tickets, double id) {
             this.date = "7.00 1 июня";
             this.time = "4 часа 30 минут";
-            if (type == 'c') {
-                this.price = 35;
-            }else if (type == 'o'){
-                this.price = 49;
-            } else if (type == 'd'){
-                this.price = 70;
-            }
+            this.price = tickets * 30 + child_tickets * 15;
+            this.id = id;
         }
     }
-    public static class ZTicket extends Ticket{
-
-        public ZTicket(char type, double id){
+    public static class ZTicket extends Ticket {
+        public ZTicket(int child_tickets, int tickets, double id) {
             this.date = "8.00 12 августа";
             this.time = "14 часов 30 минут";
-            if (type == 'c') {
-                this.price = 50;
-            }else if (type == 'o'){
-                this.price = 70;
-            } else if (type == 'd'){
-                this.price = 100;
-            }
+            this.price = tickets * 40 + child_tickets * 15;
+            this.id = id;
         }
     }
-    public static class UTicket extends Ticket{
-        public UTicket(char type, double id){
+
+    public static class UTicket extends Ticket {
+        public UTicket(int child_tickets, int tickets, double id) {
             this.date = "10.30 10 июня";
             this.time = "7 часов";
-            if (type == 'c') {
-                this.price = 30;
-            }else if (type == 'o'){
-                this.price = 42;
-            } else if (type == 'd'){
-                this.price = 60;
-            }
+            this.price = tickets * 20 + child_tickets * 20;
+            this.id = id;
         }
     }
 }
--- app/src/main/java/com/example/tickets/TicketRegistration.java
+++ app/src/main/java/com/example/tickets/TicketRegistration.java
@@ -3,30 +3,69 @@ package com.example.tickets;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 public class TicketRegistration extends AppCompatActivity {
+    String id;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ticket_registration);
         Intent intent = getIntent();
-        String id = intent.getStringExtra("keyIdButton");
-        System.out.println(id);
+        id = intent.getStringExtra("keyIdButton");
         TextView text = findViewById(R.id.description);
-        double passenger_id = (Math.random()*((999999-100000)+1))+100000;
         switch(id){
             case "2131230819":
                 text.setText(R.string.ukok);
-                break;
             case "2131230820":
                 text.setText(R.string.artibash);
-                break;
             case "2131230818":
                 text.setText(R.string.goldenring);
-                break;
         }
+        Button b = findViewById(R.id.confirm);
+        b.setOnClickListener(listener);
     }
+    private final View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            double passenger_id = (Math.random()*((999999-100000)+1))+100000;
+            EditText a = findViewById(R.id.tickets);
+            EditText b = findViewById(R.id.child_tickets);
+            int tickets = Integer.parseInt(a.getText().toString());
+            int child_tickets = Integer.parseInt(b.getText().toString());
+            Intent intent_ = new Intent(TicketRegistration.this , RegistrationDone.class);
+            switch(id){
+                case "2131230819":
+                    Ticket aticket = new Ticket.UTicket(child_tickets, tickets, passenger_id);
+                    intent_.putExtra("rout", "Укок-Аргут");
+                    intent_.putExtra("price", aticket.price);
+                    intent_.putExtra("date", aticket.date);
+                    intent_.putExtra("time", aticket.time);
+                    intent_.putExtra("id", aticket.id);
+                    break;
+                case "2131230820":
+                    Ticket bticket = new Ticket.ATicket(child_tickets, tickets, passenger_id);
+                    intent_.putExtra("rout", "Артыбаш");
+                    intent_.putExtra("price", bticket.price);
+                    intent_.putExtra("date", bticket.date);
+                    intent_.putExtra("time", bticket.time);
+                    intent_.putExtra("id", bticket.id);
+                    break;
+                case "2131230818":
+                    Ticket cticket = new Ticket.ZTicket(child_tickets, tickets, passenger_id);
+                    intent_.putExtra("rout", "Золотое Кольцо Алтая");
+                    intent_.putExtra("price", cticket.price);
+                    intent_.putExtra("date", cticket.date);
+                    intent_.putExtra("time", cticket.time);
+                    intent_.putExtra("id", cticket.id);
+                    break;
+            }
+            startActivity(intent_);
+        }
+    };
 }
